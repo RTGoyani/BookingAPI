acd9ff640a2b1f46a69f8ba0bc4e6be972694a44
goyan <goyan@DESKTOP-MG67Q84>
2024-08-01 11:37:55 +0530

    Add project files.

---
+++ BookingAPI/Controllers/BookingController.cs
@@ -0,0 +1,33 @@
+﻿using BookingAPI.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly BookingApiService _bookingApiService;
+
+        public BookingController(BookingApiService bookingApiService)
+        {
+            _bookingApiService = bookingApiService;
+        }
+
+        [HttpGet("token")]
+        public async Task<IActionResult> GetToken()
+        {
+            var token = await _bookingApiService.GenerateTokenAsync();
+            return Ok(new { Token = token });
+        }
+
+        [HttpGet("cities")]
+        public async Task<IActionResult> GetCities()
+        {
+            var token = await _bookingApiService.GenerateTokenAsync();
+            var cities = await _bookingApiService.GetCitiesAsync(token);
+            return Ok(cities);
+        }
+    }
+}
---
+++ BookingAPI/Service/BookingApiService.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace BookingAPI.Service
+{
+    public class BookingApiService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
+
+        public BookingApiService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task<string> GenerateTokenAsync()
+        {
+            var clientId = _configuration["BookingApi:ClientId"];
+            var clientSecret = _configuration["BookingApi:ClientSecret"];
+
+            var authToken = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authToken);
+
+            var response = await _httpClient.PostAsync("https://api.booking.com/oauth/token", new StringContent("grant_type=client_credentials", Encoding.UTF8, "application/x-www-form-urlencoded"));
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(content);
+
+            return tokenResponse.AccessToken;
+        }
+
+        public async Task<List<string>> GetCitiesAsync(string token)
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await _httpClient.GetAsync("https://api.booking.com/cities");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var citiesResponse = JsonConvert.DeserializeObject<CitiesResponse>(content);
+
+            return citiesResponse.Cities;
+        }
+    }
+
+    public class TokenResponse
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+
+    public class CitiesResponse
+    {
+        [JsonProperty("cities")]
+        public List<string> Cities { get; set; }
+    }
+}
